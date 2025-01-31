4bd0c5ca1865ff84486fe4d79de4ddc21b7497bc
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-31 17:52:14 +0700

    feat: update handleSubmit(), so it also add WeatherInfoCard component to WeatherInfoCars

--- src/utility/handlers.ts
+++ src/utility/handlers.ts
@@ -1,6 +1,16 @@
 import * as utils from "./utils";
+import { WeatherInfoCard } from "../components/weather-info-card";
 
 export const handleSubmit = (): void => {
+  const input = document.querySelector("input[name='location']") as HTMLInputElement;
+
+  if (utils.checkExistence(input)) {
+    console.log(input.value);
+    utils.addCardToWeatherInfoCards(new WeatherInfoCard().create(input.value));
+  } else {
+    console.warn("input not found");
+  }
+
   utils.showInputValue();
   utils.stopFormRefreshing();
 };
