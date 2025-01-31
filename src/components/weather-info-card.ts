0f9435754155eb426aa20d7fc81c7b168528c3a3
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-31 17:21:50 +0700

    feat: create InformationList component

--- src/components/weather-info-card.ts
+++ src/components/weather-info-card.ts
@@ -5,10 +5,31 @@ export class WeatherInfoCard {
     const weatherInfoCard = document.createElement("div");
     weatherInfoCard.setAttribute("class", "weather-info-card");
 
+    return weatherInfoCard;
+  }
+}
+
+export class InformationList {
+  create(): HTMLElement {
+    const ul = document.createElement("ul");
+    ul.setAttribute("class", "weather-info-list");
+
+    const city = document.createElement("li");
+    city.className = "city";
+
+    const description = document.createElement("li");
+    description.className = "description";
+
+    const timezone = document.createElement("li");
+    timezone.className = "timezone";
+
     utils.appendChildrenToParent(
-      weatherInfoCard,
+      ul,
+      city,
+      description,
+      timezone,
     );
 
-    return weatherInfoCard;
+    return ul;
   }
 }
