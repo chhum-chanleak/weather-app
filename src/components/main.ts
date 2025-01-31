c5240c1971e2520a0f2110ca502b72d209091161
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-31 09:30:13 +0700

    feat: update Main class, so it also has a constructor

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,6 +1,18 @@
+import { appendChildrenToParent } from "../utility/utils";
+import { Form } from "./form";
+
 export class Main {
+  constructor(
+    private children: HTMLElement[]
+  ) {}
+
   create(): HTMLElement {
     const main = document.createElement("main");
+
+    appendChildrenToParent(
+      main,
+      ...this.children,
+    );
 
     return main;
   }
