f33b23634c1b2af377040bc4d6f1b6a9fa73575e
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-31 07:55:51 +0700

    feat: define currentConditons, Day and WeatherInfo types

--- src/utility/utils.ts
+++ src/utility/utils.ts
@@ -1,4 +1,4 @@
-export const fetchWeatherDataFromLocation = async (location: string): Promise<void> => {
+export const fetchWeatherDataFromLocation = async (location: string): Promise<WeatherInfo | null> => {
   try {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=58BNYSLW5JBTD7K5WWJ46YRBL`);
 
@@ -7,9 +7,54 @@ export const fetchWeatherDataFromLocation = async (location: string): Promise<vo
     }
 
     const data = await response.json();
-  
-    console.log("Weather Data:", data);
+    
+    console.log(JSON.stringify(getWeatherData(data)));
+
+    return getWeatherData(data);
   } catch(error) {
     console.log("Failed to fetch weather data:", error);
+
+    return null;
   }
+};
+
+type CurrentConditions = {
+  datetime: string;
+  temp: number;
+  feelslike: number;
+  humidity: number;
+  pressure: number;
+  snow: number;
+  snowdepth: number;
+  sunrise: string;
+  sunset: string;
+  visibility: number;
+  windspeed: number;
+  source: string;
+};
+
+type Day = {
+  cloudcover: number;
+  conditions: string;
+  datetime: string;
+  description: string;
+  feelslike: number;
+  humidity: number;
+  pressure: number;
+  snow: number;
+  snowdepth: number;
+  sunrise: string;
+  sunset: string;
+  temp: number;
+  visibility: number;
+  windspeed: number;
+  source: string;
+};
+
+type WeatherInfo = {
+  city: string,
+  currentConditions: CurrentConditions,
+  days: Day[];
+  description: string;
+  timezone: string;
 };
