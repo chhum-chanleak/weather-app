254a3208dcf52b257baf9ba18328488e086d96b1
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-31 12:19:44 +0700

    feat: initialize implementation of applyEvents()

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import * as utils from "./utility/utils";
 import { Main } from "./components/main";
-import { Form } from "./components/form";
+import { FormWeather } from "./components/form";
+import { handleSubmit } from "./utility/handlers";
 
 // fetchWeatherDataFromLocation("phnom_penh");
+
+// Apply event listeners
+const applyEvents = (): void => {
+  const submitButton = document.querySelector("button[name='location']") as HTMLButtonElement;
+  submitButton.addEventListener("click", handleSubmit);
+};
 
 const app = () => {
   const container = document.querySelector(".container") as HTMLElement;
@@ -10,9 +17,12 @@ const app = () => {
   utils.appendChildrenToParent(
     container,
     new Main([
-      new Form().create(),
+      new FormWeather().create(),
     ]).create(),
   );
+
+  // Apply event listeners
+  applyEvents();
 };
 
 app();
