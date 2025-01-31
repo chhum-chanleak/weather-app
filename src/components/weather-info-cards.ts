1c81b60710fa03a8db1869b96f3633a47f0ac0a6
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-31 17:09:27 +0700

    feat: add WeatherInfoCard component to WeatherInfoCards

--- src/components/weather-info-cards.ts
+++ src/components/weather-info-cards.ts
@@ -1,7 +1,15 @@
+import * as utils from "../utility/utils";
+import { WeatherInfoCard } from "./weather-info-card";
+
 export class WeatherInfoCards {
   create(): HTMLElement {
     const weatherInfoCards = document.createElement("div");
     weatherInfoCards.setAttribute("class", "weather-info-cards");
+
+    utils.appendChildrenToParent(
+      weatherInfoCards,
+      new WeatherInfoCard().create(),
+    );
 
     return weatherInfoCards
   }
