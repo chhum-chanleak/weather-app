0d091b22383af21db3e291c4742b8c2ece6b1026
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-02-01 16:43:49 +0700

    fix: reject input value when the input value already exists

--- src/utility/handlers.ts
+++ src/utility/handlers.ts
@@ -1,16 +1,28 @@
 import * as utils from "./utils";
 import { WeatherInfoCard } from "../components/weather-info-card";
+import { searchHistoryStorage } from "../data/data";
 
 export const handleSubmit = (): void => {
   const input = document.querySelector("input[name='location']") as HTMLInputElement;
   const filteredValue = utils.filterInputValue(input.value);
 
+  // Reject input value when the input value already exists
+  // Example: You cannot fetch 'Paris' when you have already fetched it once.
+  try {
+    utils.rejectAlreadyExistsInputValue(filteredValue);
+  } catch(error) {
+    console.log(error);
+    return;
+  }
+
   // Check whether input exists and input.value is not empty
   if (utils.checkExistence(input) && filteredValue !== "") {
-    utils.addCardToWeatherInfoCards(new WeatherInfoCard().create(filteredValue));
-    utils.stopFormRefreshing();
-    utils.fillWeatherInfoCardWithInformation();
-    utils.handleLoading();
+      utils.addCardToWeatherInfoCards(new WeatherInfoCard().create(filteredValue));
+      utils.stopFormRefreshing();
+      utils.fillWeatherInfoCardWithInformation();
+      utils.handleLoading();
+      searchHistoryStorage.register(filteredValue, filteredValue);
+      console.log(searchHistoryStorage.getSearchHistoryList());
 
     // Clear input field
     input.value = "";
