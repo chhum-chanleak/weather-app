3ad9db03be48a5202d092ad3e262732d6142f66f
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-02-01 17:35:40 +0700

    feat: update rejectAlreadyExistsInputValue(), so it shows a reject message when the input value already exists

--- src/utility/utils.ts
+++ src/utility/utils.ts
@@ -197,18 +197,20 @@ export const handleLoading = () => {
 // Example: You cannot fetch 'Paris' when you have already fetched it once.
 export const rejectAlreadyExistsInputValue = (inputValue: string):void => {
   if (searchHistoryStorage.getSearchHistoryList().has(inputValue)) {
+    // Show a reject message when the input value already exists
+    setRejectMessageTextContent();
     throw new Error(`${inputValue} already exists`);
   }
 }
 
-// Set text content of rejectMessage
+// Set text content for rejectMessage
 const setRejectMessageTextContent = () => {
   const rejectMessage = document.querySelector("span.reject-message") as HTMLElement;
   const input = document.querySelector("input[name='location']") as HTMLInputElement;
   const filteredValue = filterInputValue(input.value);
 
   if (rejectMessage) {
-    rejectMessage.textContent = `'${filteredValue}' already exists. Search another location`;
+    rejectMessage.textContent = `'${filteredValue}' already exists. Search another location.`;
   } else {
     console.warn("rejectMessage not found");
   }
