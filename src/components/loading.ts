73214db3ac96e075f4369dac9d2dcd480c170595
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-02-01 12:33:54 +0700

    feat: create Loading component

---
+++ src/components/loading.ts
@@ -0,0 +1,31 @@
+import * as utils from "../utility/utils";
+
+export class Loading {
+  create(): HTMLElement {
+    const loading = document.createElement("div");
+    loading.setAttribute("class", "loading")
+
+    const loadingText = document.createElement("span");
+    loadingText.classList.add("loading-text");
+    loadingText.textContent = "Loading";
+
+    const circlesContainer = document.createElement("div");
+    circlesContainer.classList.add("circles-container");
+
+    // Create 3 circles inside loading component
+    for (let i = 0; i < 5; i += 1) {
+      const circle = document.createElement("div");
+      circle.classList.add("circle");
+
+      circlesContainer.appendChild(circle);
+    }
+
+    utils.appendChildrenToParent(
+      loading,
+      loadingText,
+      circlesContainer,
+    );
+
+    return loading;
+  }
+}
