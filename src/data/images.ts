2409d3ca8ce70f1a5b074a03e067c96277c5f749
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-02-02 20:20:14 +0700

    feat: add overcast-weather.svg and partly-cloudy-weather.svg

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,8 +1,9 @@
 import clear from "../assets/images/clear-weather.svg";
-import cloudy from "../assets/images/cloudy-weather.svg";
+import cloudy from "../assets/images/partly-cloudy-weather.svg";
 import rain from "../assets/images/rain-weather.svg";
 import snow from "../assets/images/snow-weather.svg";
 import thunder from "../assets/images/thunder-weather.svg";
+import overcast from "../assets/images/overcast-weather.svg";
 
 export const weatherImageSources = {
   clear,
