cefcf79ca3fa9600306d4502be96ff4f0d18971b
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-02-02 15:12:29 +0700

    feat: create weatherImageSources which contains paths of images

---
+++ src/data/images.ts
@@ -0,0 +1,13 @@
+import clear from "../assets/images/clear-weather.svg";
+import cloudy from "../assets/images/cloudy-weather.svg";
+import rain from "../assets/images/rain-weather.svg";
+import snow from "../assets/images/snow-weather.svg";
+import thunder from "../assets/images/thunder-weather.svg";
+
+export const weatherImageSources = {
+  clear,
+  cloudy,
+  rain,
+  snow,
+  thunder,
+};
